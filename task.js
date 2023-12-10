a20d939577597dba3dc379604c0a75c3d60e3a89
Yuri Shornikov <dbonlyfun@gmail.com>
2023-12-10 14:17:07 +0300

    it's works

--- task.js
+++ task.js
@@ -1,8 +1,8 @@
 const font__size = document.querySelectorAll('.font-size');
 const book = document.querySelector('.book');
-const color = document.querySelectorAll('.color');
 const book__content = document.querySelector('.book__content');
-
+const color_text = document.querySelectorAll('[data-text-color].color');
+const color_bg = document.querySelectorAll('[data-bg-color].color');
 
 const font_size_active = 'font-size_active';
 const color_active = 'color_active';
@@ -11,12 +11,14 @@ const func = function(element, active) {
     element.forEach((index) => {
         index.addEventListener('click', (ev) => {
             ev.preventDefault();
+    
             // удаляем активные классы и размер шрифта
             element.forEach((el_i) => {
                 el_i.classList.remove(active);
                 book.classList.remove('font-size_small');
                 book.classList.remove('font-size_big');
             })
+
             // проверяем размер и устанавливаем шрифт
             if (index.classList.contains('font-size_big')) {
                 book.classList.add('font-size_big');
@@ -24,15 +26,23 @@ const func = function(element, active) {
             if (index.classList.contains('font-size_small')) {
                 book.classList.add('font-size_small');
             }
+
             // изменяем цвет и фон 
-            book__content.style.color = index.getAttribute('data-text-color');
-            book__content.style.background = index.getAttribute('data-bg-color');
+            if (index.hasAttribute('data-text-color')) {
+                book__content.style.color = index.getAttribute('data-text-color');
+            }
+            if (index.hasAttribute('data-bg-color')) {
+                book__content.style.background = index.getAttribute('data-bg-color');  
+            }
+
             index.classList.add(active);
         })
     })
+      
 }
 // вызов функций
-func(color, color_active);
+func(color_text, color_active);
+func(color_bg, color_active);
 func(font__size, font_size_active);
 
 
